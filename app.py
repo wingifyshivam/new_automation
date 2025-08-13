8b8c421deccbf4d612eb1c83835be9bdb27f0a39
wingifyshivam <54325421+wingifyshivam@users.noreply.github.com>
2025-08-13 17:49:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.utils import secure_filename
 import os
+from lab_removal import run_lab_removal
 
 # Initialize app
 app = Flask(__name__)
